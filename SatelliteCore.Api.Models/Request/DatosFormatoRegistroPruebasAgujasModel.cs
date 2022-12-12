40ed5e759b4598692c2121a89533a7bf5cac785e
Eddie Gomez <eddiegomez@unilene.com>
2022-12-12 08:45:52 -0500

    Nuevo proyecto

--- SatelliteCore.Api.Models/Request/DatosFormatoRegistroPruebasAgujasModel.cs
+++ SatelliteCore.Api.Models/Request/DatosFormatoRegistroPruebasAgujasModel.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace SatelliteCore.Api.Models.Request
 {
     public class DatosFormatoRegistroPruebasAgujasModel
     {
-        public string Especialidad { get; set;  }
+        public string Especialidad { get; set; }
         public string Lote { get; set; }
         public List<GuardarPruebaFlexionAgujaModel> Detalle { get; set; }
     }
